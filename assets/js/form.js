52d61076b10106e384cebad22da76bc9a1da2782
nikiljos <nikhiljose12345@gmail.com>
2021-12-23 17:18:44 +0530

    connected amiling list backend, fixed submti button style

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,7 +1,12 @@
-function submitForm(){
+const form = document.getElementById('contact-form');
+form.addEventListener('submit', contactForm);
+
+const mlist = document.getElementById('ml-form');
+mlist.addEventListener('submit', mlistForm);
+
+function contactForm(){
 
     document.getElementById("status").innerText="Loading..."
-
     let data={
         name:document.getElementById("name").value,
         email:document.getElementById("email").value,
@@ -9,8 +14,7 @@ function submitForm(){
         message:document.getElementById("message").value,
 
     }
-
-    
+  
     axios({
         method: 'post',
         url: 'https://compasscek.herokuapp.com/contactform',
@@ -24,6 +28,26 @@ function submitForm(){
         document.getElementById("status").innerText="Some Error Occured... Please try again"
       });
       
+}
 
+function mlistForm(){
 
+    document.getElementById("ml-status").innerText="Loading..."
+    let data={
+        email:document.getElementById("ml-email").value,
+    }
+  
+    axios({
+        method: 'post',
+        url: 'https://compasscek.herokuapp.com/mlist',
+        data: data
+      })
+      .then((response) => {
+        console.log(response.data);
+        document.getElementById("ml-status").innerText=response.data.message
+      }, (error) => {
+        console.log(error);
+        document.getElementById("ml-status").innerText="Some Error Occured... Please try again"
+      });
+      
 }
